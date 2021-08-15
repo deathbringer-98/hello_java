dd3e4a788875a610f291dc2d0451a176667eb41a
deathbringer-98 <classicfun88@gmail.com>
2021-08-15 21:28:50 +0530

    fixed semicolon/added scm trigger

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World!");
-        System.out.println("Onkar Jaliminche")
+        System.out.println("Onkar Jaliminche");
+        System.out.println("Onkar Siddheshwar Jaliminche");
     }
 }
